fbc2ee9136a22cdc9c481b6bcdd6c56f8144cd02
TheoMoriceEM <theo.morice5@gmail.com>
2023-03-26 00:01:32 +0100

    Made form with currency and dates fields

--- resources/views/quotation-form.blade.php
+++ resources/views/quotation-form.blade.php
@@ -8,10 +8,33 @@
     </head>
     <body class="antialiased">
         <div class="container">
-            <h1>Enter your information below to get a quotation for your trip :</h1>
+            <h1 class="my-4">Enter your information below to get a quotation for your trip :</h1>
             <form method="POST" action="{{ route('quotation-form') }}">
                 @csrf
 
+                <div class="row mb-3">
+                    <label for="currency" class="col-sm-1 col-form-label">Currency :</label>
+                    <div class="col-sm-3">
+                        <select class="form-select" id="currency" name="currency" required>
+                            <option disabled selected>Choose a currency for your quotation</option>
+                            <option value="EUR">EUR</option>
+                            <option value="GBP">GBP</option>
+                            <option value="USD">USD</option>
+                        </select>
+                    </div>
+
+                    <label for="start_date" class="col-sm-1 col-form-label">Start date :</label>
+                    <div class="col-sm-3">
+                        <input type="date" id="start_date" name="start_date" class="form-control" required>
+                    </div>
+
+                    <label for="end_date" class="col-sm-1 col-form-label">End date :</label>
+                    <div class="col-sm-3">
+                        <input type="date" id="end_date" name="end_date" class="form-control" required>
+                    </div>
+                </div>
+
+                <button type="submit" class="btn btn-primary">Generate a quotation</button>
             </form>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
